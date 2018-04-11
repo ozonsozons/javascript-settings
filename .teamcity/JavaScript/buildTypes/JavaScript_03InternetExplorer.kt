e002dd828367e6574e6f2aad7c99d973d35089db
ozonsozons <ozonsozons@example.com>
2018-04-11 15:30:50 +0300

    additional changes

--- .teamcity/JavaScript/buildTypes/JavaScript_03InternetExplorer.kt
+++ .teamcity/JavaScript/buildTypes/JavaScript_03InternetExplorer.kt
@@ -4,12 +4,12 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.*
 
 object JavaScript_03InternetExplorer : BuildType({
     template(JavaScript.buildTypes.JavaScript_Template1)
-    uuid = "d0f9a8a9-8613-4025-9c40-28c432ecd4be"
+    uuid = "javascripttest_03_InternetExplorer"
     id = "JavaScript_03InternetExplorer"
     name = "03. InternetExplorer"
 
     params {
-        param("Browser", "Internet Explorer")
+        param("Browser", "IE")
     }
 
     dependencies {
