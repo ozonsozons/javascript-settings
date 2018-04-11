587c92af0aa5cbbac0288984b6833c1e0c3f3410
ozonsozons <ozonsozons@example.com>
2018-04-11 15:22:41 +0300

    Changing file

--- .teamcity/JavaScript/buildTypes/JavaScript_03InternetExplorer.kt
+++ .teamcity/JavaScript/buildTypes/JavaScript_03InternetExplorer.kt
@@ -2,14 +2,14 @@ package JavaScript.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
 
-object JavaScript_03Firefox : BuildType({
+object JavaScript_03InternetExplorer : BuildType({
     template(JavaScript.buildTypes.JavaScript_Template1)
     uuid = "d0f9a8a9-8613-4025-9c40-28c432ecd4be"
-    id = "JavaScript_03Firefox"
-    name = "03. Firefox"
+    id = "JavaScript_03InternetExplorer"
+    name = "03. InternetExplorer"
 
     params {
-        param("Browser", "Firefox")
+        param("Browser", "Internet Explorer")
     }
 
     dependencies {
